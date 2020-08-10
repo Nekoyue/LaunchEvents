de587e15acb8a5703e0258d6b3930670fcb945ee
Kagurazaka Tsuki <46295491+Nekoyue@users.noreply.github.com>
2020-08-10 16:56:23 +0800

    Fix time format, always uses hh:mm:ss Also fix countdown time

--- src/main/kotlin/moe/yue/launchlib/Utils.kt
+++ src/main/kotlin/moe/yue/launchlib/Utils.kt
@@ -1,19 +1,42 @@
 package moe.yue.launchlib
 
+import org.jetbrains.kotlin.utils.doNothing
 import java.time.Instant
+import java.time.LocalTime
 import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import kotlin.math.roundToInt
 
 val timeUtils = TimeUtils()
 
 class TimeUtils {
-    // Convert epoch time to ISO-8601 Time, such as "2020-08-08T15:55:27Z[UTC]"
-    fun toTime(epochTime: Long, timeZone: String = "UTC") =
-        Instant.ofEpochSecond(epochTime).atZone(ZoneId.of(timeZone)).toString()
+    // Convert epoch time to ISO-8601 UTC Date and Time, such as "2020-08-08 15:55:27"
+    fun toTime(epochTime: Long, timeZone: String = "UTC"): String =
+        Instant.ofEpochSecond(epochTime).atZone(ZoneId.of(timeZone))
+            .run {
+                this.format(DateTimeFormatter.ISO_ZONED_DATE_TIME)
+                    .replaceFirst("T", " ")
+                    .replaceFirst("Z[UTC]", "")
+            }
 
-    // Convert epoch time to simple ISO-8601 Date, such as "2020-08-08"
-    fun toSimpleDate(epochTime: Long, timeZone: String = "UTC") =
-        Instant.ofEpochSecond(epochTime).atZone(ZoneId.of(timeZone)).toLocalDate().toString()
+    // Convert epoch time to simple ISO-8601 UTC Date, such as "2020-08-08"
+    fun toSimpleDate(epochTime: Long, timeZone: String = "UTC"): String =
+        Instant.ofEpochSecond(epochTime).atZone(ZoneId.of(timeZone)).toLocalDate()
+            .run { this.format(DateTimeFormatter.ISO_DATE) }
+
+    // Convert seconds to time period, such as "15:55:27" and "6 days, 5:43:21"
+    fun toCountdownTime(seconds: Int): String = toCountdownTime(seconds.toLong())
+    fun toCountdownTime(seconds: Long): String {
+        var result = ""
+        when (val days = seconds / daysToSeconds(1)) {
+            0L -> doNothing()
+            1L -> result += "1 day, "
+            else -> result += "$days days, "
+        }
+        result += LocalTime.MIN.plusSeconds(seconds).run { this.format(DateTimeFormatter.ISO_TIME).removePrefix("0") }
+        return result
+    }
+
 
     // Convert time to epoch time
     fun toEpochTime(time: String?): Long? = if (time.isNullOrEmpty()) toEpochTime(time) else null
