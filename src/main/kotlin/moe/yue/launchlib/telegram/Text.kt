627af19cb9afa18d51f72990d025d5f932c64234
Kagurazaka Tsuki <46295491+Nekoyue@users.noreply.github.com>
2020-09-06 01:18:07 +0800

    Regex: do not replace markdown in urls

--- src/main/kotlin/moe/yue/launchlib/telegram/Text.kt
+++ src/main/kotlin/moe/yue/launchlib/telegram/Text.kt
@@ -12,11 +12,12 @@ import java.time.ZoneId
 infix fun String?.add(string: String?) = if (this.isNullOrEmpty() || string.isNullOrEmpty()) "" else "$this$string"
 
 // Convert MarkdownV2 to HTML, inspired by https://gist.github.com/jbroadway/2836900
+// I'm not exactly sure how it works, just messing around...
 fun String.toHTML() = this
     .replace(Regex("\\[(.*)\\]\\(([^\\)]+)\\)")) { "<a href='${it.groupValues[2]}'>${it.groupValues[1]}</a>" } // links
-    .replace(Regex("(__)(.*?[^\\\\\\n])\\1")) { "<u>${it.groupValues[2].replace("\\_", "_")}</u>" } // underline
+    .replace(Regex("(^| )(?!http)(.?)(__)(.*?[^\\\\\\n])\\3")) { "${it.groupValues[1]}${it.groupValues[2]}<u>${it.groupValues[4].replace("\\_", "_")}</u>" } // underline
     .replace(Regex("(\\*)(.*?[^\\\\\\n])\\1")) { "<b>${it.groupValues[2].replace("\\*", "*")}</b>" } // bold
-    .replace(Regex("(_)(.*?[^\\\\\\n])\\1")) { "<i>${it.groupValues[2].replace("\\_", "_")}</i>" } // italic
+    .replace(Regex("(^| )(?!http)(.?)(_)(.*?[^\\\\\\n])\\3")) { "${it.groupValues[1]}${it.groupValues[2]}<i>${it.groupValues[4].replace("\\_", "_")}</i>" } // italic
     .replace(Regex("\\~(.*?[^\\\\])\\~")) { "<s>${it.groupValues[1].replace("\\~", "~")}</s>" } // strike
     .replace(Regex("\\`\\`\\`(.*\\n[\\s\\S]*?\\n)\\`\\`\\`")) { "<pre>${it.groupValues[1]}</pre>" } // code block
     .replace(Regex("(\\`)(.*?[^\\\\\\n])\\1")) {
