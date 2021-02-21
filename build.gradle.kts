335281adc1de664b688b1c93c9b44343ae0d111d
nekoyue <cirno.yue@gmail.com>
2021-02-21 23:31:37 +0800

    Bump version to Kotlin 1.4.30 & Gradle 6.8.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.30-M1"
-    kotlin("plugin.serialization") version "1.4.30-M1"
+    kotlin("jvm") version "1.4.30"
+    kotlin("plugin.serialization") version "1.4.30"
     id("com.github.johnrengelman.shadow") version "6.1.0"
     application
 }
@@ -22,20 +22,20 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
 
     // Http
-    implementation("io.ktor:ktor-client-okhttp:1.5.0")
-    implementation("io.ktor:ktor-client-serialization-jvm:1.5.0")
-    implementation("io.ktor:ktor-client-logging-jvm:1.5.0")
+    implementation("io.ktor:ktor-client-okhttp:1.5.1")
+    implementation("io.ktor:ktor-client-serialization-jvm:1.5.1")
+    implementation("io.ktor:ktor-client-logging-jvm:1.5.1")
     implementation("org.slf4j:slf4j-simple:1.7.30")
     implementation("io.github.microutils:kotlin-logging:2.0.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.1.0")
 
     // Database
-    implementation("org.jetbrains.exposed:exposed-core:0.28.1")
-    implementation("org.jetbrains.exposed:exposed-jdbc:0.28.1")
+    implementation("org.jetbrains.exposed:exposed-core:0.29.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.29.1")
     implementation("com.h2database:h2:1.4.200")
 
     // Config
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-hocon:1.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-hocon:1.1.0")
 
     testImplementation(kotlin("test-junit"))
 }
@@ -45,6 +45,8 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn" // Required by ConfigParser.kt
 }
 application {
+    // mainClass.set("moe.yue.launchlib.MainKt")
+    // Deprecated, ShadowJar merge & release pending https://github.com/johnrengelman/shadow/pull/612
     mainClassName = "moe.yue.launchlib.MainKt"
 }
 tasks.withType<ShadowJar> {
